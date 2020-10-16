53e8fa638e4bbfb6b2aaa41fe2baedaeaa7564cc
IldarGabi <IldarGabi@gmail.com>
2020-10-16 03:49:37 +0300

    Lombok

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,99 +1,112 @@
 package ru.netology;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Radio {
     private long currentRadioStationNumber;
-    private long maxRadioStation;
-    private long minRadioStation;
-    private long maxVolume;
-    private long minVolume;
+    private long maxRadioStation = 10;
+    private long minRadioStation = 0;
+    private long maxVolume = 100;
+    private long minVolume = 0;
     private long currentVolume;
 
-    public long getCurrentRadioStationNumber() {
-        return currentRadioStationNumber;
-    }
+//    public Radio(long currentRadioStationNumber, long maxRadioStation, long minRadioStation, long maxVolume, long minVolume, long currentVolume) {
+//        this.currentRadioStationNumber = currentRadioStationNumber;
+//        this.maxRadioStation = maxRadioStation;
+//        this.minRadioStation = minRadioStation;
+//        this.maxVolume = maxVolume;
+//        this.minVolume = minVolume;
+//        this.currentVolume = currentVolume;
+//    }
 
-    public void setCurrentRadioStationNumber(long currentRadioStationNumber) {
-        if (currentRadioStationNumber > maxRadioStation) {
-            currentRadioStationNumber=maxRadioStation;
-        }
-        if (currentRadioStationNumber < minRadioStation) {
-            currentRadioStationNumber=minRadioStation;
-        }
+        public Radio(long currentRadioStationNumber, long currentVolume) {
         this.currentRadioStationNumber = currentRadioStationNumber;
-    }
-
-    public long getMaxRadioStation() {
-        return maxRadioStation;
-    }
-
-    public void setMaxRadioStation(long maxRadioStation) {
-        this.maxRadioStation = maxRadioStation;
-    }
-
-    public long getMinRadioStation() {
-        return minRadioStation;
-    }
-
-    public void setMinRadioStation(long minRadioStation) {
-        this.minRadioStation = minRadioStation;
-    }
-
-    public long getMaxVolume() {
-        return maxVolume;
-    }
-
-    public void setMaxVolume(long maxVolume) {
-        this.maxVolume = maxVolume;
-    }
-
-    public long getMinVolume() {
-        return minVolume;
-    }
-
-    public void setMinVolume(long minVolume) {
-        this.minVolume = minVolume;
-    }
-
-    public long getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(long currentVolume) {
-        if (currentVolume > maxVolume) {
-            currentVolume = maxVolume;
-        }
-        if (currentVolume < minVolume) {
-            currentVolume = minVolume;
-        }
         this.currentVolume = currentVolume;
     }
+
+//    public long getCurrentRadioStationNumber() {
+//        return currentRadioStationNumber;
+//    }
+//
+//    public void setCurrentRadioStationNumber(long currentRadioStationNumber) {
+//        this.currentRadioStationNumber = currentRadioStationNumber;
+//    }
+//
+//    public long getMaxRadioStation() {
+//        return maxRadioStation;
+//    }
+//
+//    public void setMaxRadioStation(long maxRadioStation) {
+//        this.maxRadioStation = maxRadioStation;
+//    }
+//
+//    public long getMinRadioStation() {
+//        return minRadioStation;
+//    }
+//
+//    public void setMinRadioStation(long minRadioStation) {
+//        this.minRadioStation = minRadioStation;
+//    }
+//
+//    public long getMaxVolume() {
+//        return maxVolume;
+//    }
+//
+//    public void setMaxVolume(long maxVolume) {
+//        this.maxVolume = maxVolume;
+//    }
+//
+//    public long getMinVolume() {
+//        return minVolume;
+//    }
+//
+//    public void setMinVolume(long minVolume) {
+//        this.minVolume = minVolume;
+//    }
+//
+//    public long getCurrentVolume() {
+//        return currentVolume;
+//    }
+//
+//    public void setCurrentVolume(long currentVolume) {
+//        this.currentVolume = currentVolume;
+//    }
 
     // change stations
     public void changeOnNextRadioStations() {
         if (currentRadioStationNumber == maxRadioStation) {
             currentRadioStationNumber = minRadioStation;
         } else currentRadioStationNumber++;
+        if (currentRadioStationNumber > maxRadioStation) {
+            currentRadioStationNumber = maxRadioStation;
+        }
     }
 
     public void changeOnPrevRadioStations() {
         if (currentRadioStationNumber == minRadioStation) {
             currentRadioStationNumber = maxRadioStation;
         } else currentRadioStationNumber--;
+        if (currentRadioStationNumber < minRadioStation) {
+            currentRadioStationNumber = minRadioStation;
+        }
     }
 
-    // change volume
-    public void changeVolumeIncrease() {
-        if (currentVolume <= minVolume)
-            return;
-            ++currentVolume;
 
+    // change volume
+
+    public void changeVolumeIncrease() {
+        if (currentVolume >= maxVolume) {
+            currentVolume = maxVolume;
+        } else currentVolume++;
     }
 
     public void changeVolumeDecrease() {
-        if (currentVolume >= maxVolume)
-            return;
-            --currentVolume;
-
+        if (currentVolume <= minVolume) {
+            currentVolume = minVolume;
+        } else currentVolume--;
     }
 }
 
